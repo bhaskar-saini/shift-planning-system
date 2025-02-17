ab3cd37e72d52441ec875c7eb30227ab77b82632
Bhaskar Saini <90419882+bhaskar-saini@users.noreply.github.com>
2025-02-17 23:12:45 +0530

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://shiftmate-backend-xe38.onrender.com/api",
+  baseURL: "http://localhost:5000/api",
 });
 
 API.interceptors.request.use((req) => {
